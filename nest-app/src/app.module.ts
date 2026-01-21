06403f05d63b3399fee2174aeafef676927347b7
Sunil Salaria <sunil.salaria@lnpinfotech.com>
2026-01-21 17:35:34 +0530

    module Ui

--- nest-app/src/app.module.ts
+++ nest-app/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductService } from './product/product.service';
 import { ProductController } from './product/product.controller';
+import { EmployeeModule } from './employee/employee.module';
 
 @Module({
-  imports: [],
+  imports: [EmployeeModule],
   controllers: [AppController, ProductController],
   providers: [AppService, ProductService],
 })
